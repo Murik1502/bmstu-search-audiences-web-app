f8083482d27bda4c62f198d0f58cd963befd8ee0
Murik1502 <nikita.murashka@gmail.com>
2024-04-04 00:33:38 +0300

    refactor: removed unused files

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import {useTelegram} from "../../hooks/useTelegram";
+import "./Footer.css"
+const FSGNLink = "https://t.me/bmstu_fsgn"
+
+
+const Footer = () => {
+    const {tg} = useTelegram();
+
+    return (
+        <div className={"footer"}>
+            <p className={"footer__desc"}>© 2024 МГТУ им. Н.Э. Баумана</p>
+            <a className={"footer__link"} href="javascript:tg.openTelegramLink(FSGNLink);">Created by SGN</a>
+        </div>
+    );
+};
+
+export default Footer;
