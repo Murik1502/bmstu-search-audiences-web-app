f4c9b5626a693cb8a9bcd9939ae7bf35d5149fb5
Nikita Murashka <144274342+Murik1502@users.noreply.github.com>
2024-04-08 21:58:27 +0300

    Update PostService.js

--- src/API/PostService.js
+++ src/API/PostService.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 export default class PostService {
-    
     static async getAll(week = "", day = "") {
         const response = await axios.get("https://roscosmosmuseum.store/data", {
             params: {
