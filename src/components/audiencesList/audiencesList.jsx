538674edbcfab8c4174cbba6575dd36402079e56
nikitamurashka <nikita.murashka@gmail.com>
2024-04-11 09:08:59 +0300

    feat: levels sort

--- src/components/audiencesList/audiencesList.jsx
+++ src/components/audiencesList/audiencesList.jsx
@@ -1,23 +1,30 @@
 import React from 'react';
 import ListItem from "../listItem/listItem";
-import "./audiencesList.css"
-
+import "./audiencesList.css";
 
 const AudiencesList = ({ items, floors }) => {
-
     return (
         <div className="audiences-list">
-            {floors.map((floor, index) => (floor.checked &&  items.filter(item => item.floor === floor.value).length !== 0 &&
-                (<div key={index} className="floor-audiences">
-                    <div className="floor">{floor.value+" этаж"}</div>
-                    <div className="audiences-line">
-                        {items.map((item, idx) =>
-                            item.floor === floor.value &&
-                            <ListItem key={index} number={item.number} time={item.time}></ListItem>
-                        )}
-                    </div>
-                </div>)
-            ))}
+            {floors.map((floor, floorIndex) => {
+                if (floor.checked === true && items.filter(item => item.floor === floor.value).length !== 0) {
+                    return (
+                        <div key={floorIndex} className="floor-audiences">
+                            <div className="floor">{floor.value + " этаж"}</div>
+                            <div className="audiences-line">
+                                {items.map((item, itemIndex) => {
+                                    if (item.floor === floor.value) {
+                                        return (
+                                            <ListItem key={itemIndex} number={item.number} time={item.time}></ListItem>
+                                        );
+                                    }
+                                    return null;
+                                })}
+                            </div>
+                        </div>
+                    );
+                }
+                return null;
+            })}
         </div>
     );
 };
--- src/pages/list.jsx
+++ src/pages/list.jsx
@@ -7,6 +7,21 @@ import {useFetching} from "../hooks/useFetching";
 import Footer from "../components/Footer/Footer";
 import AudiencesList from "../components/audiencesList/audiencesList";
 
+const audiencesArray = [
+    {floor: '6', number: "719л", time: "8:30 - 12:00"},
+    {floor: '6', number: "719л", time: "8:30 - 12:00"},
+    {floor: '6', number: "719л", time: "8:30 - 12:00"},
+    {floor: '7', number: "719л", time: "8:30 - 12:00"},
+    {floor: '7', number: "719л", time: "8:30 - 12:00"},
+    {floor: '7', number: "719л", time: "8:30 - 12:00"},
+    {floor: '7', number: "719л", time: "8:30 - 12:00"},
+    {floor: '7', number: "719л", time: "8:30 - 12:00"},
+    {floor: '8', number: "719л", time: "8:30 - 12:00"},
+    {floor: '8', number: "719л", time: "8:30 - 12:00"},
+    {floor: '8', number: "719л", time: "8:30 - 12:00"},
+    {floor: '9', number: "719л", time: "8:30 - 12:00"},
+]
+
 const levels = [
     { label: '1 этаж', value: '1', checked: true },
     { label: '2 этаж', value: '2', checked: true },
@@ -47,27 +62,26 @@ function transformData(data) {
 
 function List () {
 
-    // const location = useLocation();
-    // const navigate = useNavigate();
-    //
-    // const {toggleBackButton, tg} = useTelegram();
-    //
-    // const onBackButtonClick = () => {
-    //     tg.BackButton.offClick(onBackButtonClick);
-    //     toggleBackButton();
-    //     navigate('/');
-    // }
-    //
-    // if (!tg.BackButton.isVisible) {
-    //     toggleBackButton();
-    //     tg.BackButton.onClick(onBackButtonClick);
-    // }
+    const location = useLocation();
+    const navigate = useNavigate();
+
+    const {toggleBackButton, tg} = useTelegram();
+
+    const onBackButtonClick = () => {
+        tg.BackButton.offClick(onBackButtonClick);
+        toggleBackButton();
+        navigate('/');
+    }
+    if (!tg.BackButton.isVisible) {
+        toggleBackButton();
+        tg.BackButton.onClick(onBackButtonClick);
+    }
     //
     // //const weekDayData = location.state.data
     // //console.log("week day", weekDayData);
     //
-    // const [audiences, setAudiences] = useState([])
-    // const [sortedAudiences, setSortedAudiences] = useState(audiences)
+    const [audiences, setAudiences] = useState(audiencesArray)
+    const [sortedAudiences, setSortedAudiences] = useState(audiences)
     //
     // // eslint-disable-next-line no-unused-vars
     // const [fetchAudiences, isPostLoading, postError] = useFetching(
@@ -100,7 +114,10 @@ function List () {
 
     useEffect(() => {
         setTimeOptions(timeOptions);
-    }, [timeOptions]);
+    }, [timeOptions])
+
+    var selectedTimes = timeOptions.filter(option => option.checked).map(option => option.value);
+
     //
     // useEffect(() => {
     //     setSortedAudiences(filterAudiences(audiences, levelOptions, timeOptions))
@@ -145,28 +162,16 @@ function List () {
                             dropdownStyle={{width: "45vw", right: 0}}
                             options={null} setOptions={null}
                 />
-                <Mydropdown defaultValue={"8:30, 12:00, 13:50"}
+                <Mydropdown
                             hasImage={false}
                             buttonStyle={{padding: "13px 18px",  borderRadius: "100px", height: "45px"}}
                             textWidth={"26vw"}
                             dropdownStyle={{width: "45vw", right: 0}}
+                            defaultValue={selectedTimes.length ? selectedTimes.join(", ") : "Время"}
                             options={timeOptions} setOptions={setTimeOptions}
                 />
             </div>
-            {/*<AudiencesList items={[*/}
-            {/*    {floor: '6', number: "719л", time: "8:30 - 12:00"},*/}
-            {/*    {floor: '6', number: "719л", time: "8:30 - 12:00"},*/}
-            {/*    {floor: '6', number: "719л", time: "8:30 - 12:00"},*/}
-            {/*    {floor: '7', number: "719л", time: "8:30 - 12:00"},*/}
-            {/*    {floor: '7', number: "719л", time: "8:30 - 12:00"},*/}
-            {/*    {floor: '7', number: "719л", time: "8:30 - 12:00"},*/}
-            {/*    {floor: '7', number: "719л", time: "8:30 - 12:00"},*/}
-            {/*    {floor: '7', number: "719л", time: "8:30 - 12:00"},*/}
-            {/*    {floor: '8', number: "719л", time: "8:30 - 12:00"},*/}
-            {/*    {floor: '8', number: "719л", time: "8:30 - 12:00"},*/}
-            {/*    {floor: '8', number: "719л", time: "8:30 - 12:00"},*/}
-            {/*    {floor: '9', number: "719л", time: "8:30 - 12:00"},*/}
-            {/*]} floors={levelOptions}/>*/}
+            <AudiencesList items={audiences} floors={levelOptions}/>
             <Footer/>
         </div>
     );
