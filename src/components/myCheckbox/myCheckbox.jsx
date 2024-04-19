c2cc93d68b551bc287f0f37d676269619742bf50
nikitamurashka <nikita.murashka@gmail.com>
2024-04-20 01:14:56 +0300

    chore: remove unused vars

--- src/components/myCheckbox/myCheckbox.jsx
+++ src/components/myCheckbox/myCheckbox.jsx
@@ -3,11 +3,11 @@ import "./myCheckbox.css";
 const MyCheckbox = ({weekDay, setWeekDay}) => {
     return (
             <div className="day_selector">
-                <input type="radio" name="role" id="numerator" value="numerator" defaultChecked={true} onInput={(e) => setWeekDay({
+                <input type="radio" name="role" id="numerator" value="numerator" defaultChecked={true} onInput={() => setWeekDay({
                     ...weekDay, week: "numerator"
                 })}/>
                 <label htmlFor="numerator" className="numerator">Числитель</label>
-                <input type="radio" name="role" id="denominator" value="denominator" onInput={(e) => setWeekDay({
+                <input type="radio" name="role" id="denominator" value="denominator" onInput={() => setWeekDay({
                     ...weekDay, week: "denominator"
                 })}/>
                 <label htmlFor="denominator" className="denominator">Знаменатель</label>
