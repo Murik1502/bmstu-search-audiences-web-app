4fc779ebda06ee7d418d537d7ede1be61e66252a
nikitamurashka <nikita.murashka@gmail.com>
2024-04-19 20:45:40 +0300

    chore: classnames

--- src/components/myCheckbox/myCheckbox.jsx
+++ src/components/myCheckbox/myCheckbox.jsx
@@ -2,15 +2,15 @@ import "./myCheckbox.css";
 
 const MyCheckbox = ({weekDay, setWeekDay}) => {
     return (
-            <div className="role_selector">
-                <input type="radio" name="role" id="student" value="numerator" defaultChecked={true} onInput={(e) => setWeekDay({
+            <div className="day_selector">
+                <input type="radio" name="role" id="numerator" value="numerator" defaultChecked={true} onInput={(e) => setWeekDay({
                     ...weekDay, week: "numerator"
                 })}/>
-                <label htmlFor="student" className="student">Числитель</label>
-                <input type="radio" name="role" id="teacher" value="denominator" onInput={(e) => setWeekDay({
+                <label htmlFor="numerator" className="numerator">Числитель</label>
+                <input type="radio" name="role" id="denominator" value="denominator" onInput={(e) => setWeekDay({
                     ...weekDay, week: "denominator"
                 })}/>
-                <label htmlFor="teacher" className="teacher">Знаменатель</label>
+                <label htmlFor="denominator" className="denominator">Знаменатель</label>
             </div>
     );
 };
