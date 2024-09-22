2364d1bef567b56e5e8d59eca737c03223e11a39
nikitamurashka <nikita.murashka@gmail.com>
2024-09-22 18:14:33 +0300

    fix: telegram vh

--- src/hooks/useTelegram.js
+++ src/hooks/useTelegram.js
@@ -1,27 +1,26 @@
-const tg = window.Telegram.WebApp;
+const tg = window.Telegram.WebApp
 
 export function useTelegram() {
+	const toggleBackButton = () => {
+		if (tg.BackButton.isVisible) {
+			tg.BackButton.hide()
+		} else {
+			tg.BackButton.show()
+		}
+	}
 
-    const toggleBackButton = () => {
-        if(tg.BackButton.isVisible) {
-            tg.BackButton.hide();
-        } else {
-            tg.BackButton.show();
-        }
-    }
+	const toggleMainButton = () => {
+		if (tg.MainButton.isVisible) {
+			tg.MainButton.hide()
+		} else {
+			tg.MainButton.show()
+		}
+	}
 
-    const toggleMainButton = () => {
-        if(tg.MainButton.isVisible) {
-            tg.MainButton.hide();
-        } else {
-            tg.MainButton.show();
-        }
-    }
-
-    return {
-        toggleBackButton,
-        toggleMainButton,
-        tg,
-        user: tg.initDataUnsafe?.user,
-    }
+	return {
+		toggleBackButton,
+		toggleMainButton,
+		tg,
+		user: tg.initDataUnsafe?.user,
+	}
 }
