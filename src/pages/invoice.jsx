b6134275fd54e36c2b3ee7ced87f272e58863da1
nikitamurashka <nikita.murashka@gmail.com>
2024-09-25 23:12:05 +0300

    test: rawInitData

--- src/pages/invoice.jsx
+++ src/pages/invoice.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { retrieveLaunchParams } from '@telegram-apps/sdk'
+import { useEffect, useState } from 'react'
 import { useTelegram } from '../hooks/useTelegram'
 
 function handleInvoiceStatus(status) {
@@ -19,11 +20,65 @@ export function Invoice() {
 
 	const { tg } = useTelegram()
 
+	const { initDataRaw } = retrieveLaunchParams()
+
+	useEffect(() => {
+		console.log(initDataRaw)
+	}, [initDataRaw])
+
+	const handleButtonClick = () => {
+		navigator.clipboard
+			.writeText('initDataRaw')
+			.then(() => alert('Raw Init Data copied to clipboard!'))
+			.catch(() => alert('Failed to copy Raw Init Data to clipboard!'))
+	}
+
 	return (
-		<div>
-			<h1>Invoice</h1>
-			<input value={value} onChange={e => setValue(e.target.value)} />
+		<div
+			style={{
+				alignItems: 'center',
+				flexDirection: 'column',
+				justifyContent: 'center',
+				height: '100%',
+				width: '100vw',
+				gap: '20px',
+				display: 'flex',
+			}}
+		>
 			<button
+				style={{
+					padding: '10px',
+					background: 'transparent',
+					color: 'white',
+					border: '1px solid white',
+					borderRadius: '10px',
+					cursor: 'pointer',
+				}}
+				onClick={handleButtonClick}
+			>
+				Copy Raw Init Data
+			</button>
+			<input
+				style={{
+					padding: '10px',
+					background: 'transparent',
+					color: 'white',
+					border: '1px solid white',
+					borderRadius: '10px',
+				}}
+				value={value}
+				placeholder='Enter invoice link'
+				onChange={e => setValue(e.target.value)}
+			/>
+			<button
+				style={{
+					padding: '10px',
+					background: 'transparent',
+					color: 'white',
+					border: '1px solid white',
+					borderRadius: '10px',
+					cursor: 'pointer',
+				}}
 				onClick={() => {
 					try {
 						tg.openInvoice(value, handleInvoiceStatus)
